4f54ed7a809b5ad2da105d6597c403c2168436ee
gavinkim <gavingvi0122@gmail.com>
2023-03-06 16:21:04 +0900

    change update

--- app/src/main/java/kim/gavin/artica/Mainpage.java
+++ app/src/main/java/kim/gavin/artica/Mainpage.java
@@ -10,5 +10,8 @@ public class Mainpage extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpage);
+
+
+
     }
 }
