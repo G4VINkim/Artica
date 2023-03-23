5d6cb7058b46622b4d0ab5792196a4435c6a31ea
gavinkim <gavingvi0122@gmail.com>
2023-03-23 18:57:08 -0400

    change update

---
+++ app/src/main/java/kim/gavin/artica/Login.java
@@ -0,0 +1,15 @@
+package kim.gavin.artica;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginpage);
+    }
+}
+
--- app/src/main/java/kim/gavin/artica/Mainpage.java
+++ app/src/main/java/kim/gavin/artica/Mainpage.java
@@ -1,6 +1,9 @@
 package kim.gavin.artica;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +14,14 @@ public class Mainpage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpage);
 
-
+        Button btn = findViewById(R.id.login_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(Mainpage.this, Page2.class);
+                startActivity(it);
+            }
+        });
 
     }
 }
