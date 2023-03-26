97681a3e8ecb19e4f3f2e7c22e65dfd73ca60d37
gavinkim <gavingvi0122@gmail.com>
2023-03-25 21:56:03 -0400

    change update

--- app/src/main/java/kim/gavin/artica/Aboutus.java
+++ app/src/main/java/kim/gavin/artica/Aboutus.java
@@ -10,5 +10,4 @@ public class Aboutus extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aboutus);
-    }
 }
