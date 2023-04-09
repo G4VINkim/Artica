ea34024dbaf2fe9f21e7c87538749608dca0f302
gavinkim <gavingvi0122@gmail.com>
2023-04-09 19:51:55 -0400

    change update

--- app/src/main/java/kim/gavin/artica/Form.java
+++ app/src/main/java/kim/gavin/artica/Form.java
@@ -1,14 +1,79 @@
 package kim.gavin.artica;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Form extends AppCompatActivity {
+public class Form extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    ImageView imageView;
+    Button upload_btn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form);
+        upload_btn = (Button) findViewById(R.id.button9);
+        imageView = findViewById(R.id.imageView12);
+        upload_btn.setOnClickListener(v -> mgetContent.launch("image/*"));
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner4);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner5);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
+
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner6);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+        spinner3.setOnItemSelectedListener(this);
+
+    }
+    ActivityResultLauncher<String> mgetContent = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>(){
+                @Override
+                public void onActivityResult(Uri result) {
+                    if (result != null) {
+                        imageView.setImageURI(result);
+                        imageView.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+    );
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
