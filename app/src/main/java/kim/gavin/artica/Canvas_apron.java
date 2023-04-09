37ec5f88c62a703eb79bcbfe772c1691b524e535
serajang99 <serajang1679@gmail.com>
2023-04-09 14:27:59 +0900

    error fix

--- app/src/main/java/kim/gavin/artica/Canvas_apron.java
+++ app/src/main/java/kim/gavin/artica/Canvas_apron.java
@@ -12,51 +12,43 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Canvas_apron extends AppCompatActivity {
+public class Canvas_apron extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.canvas_apron);
 
-        public class Matchpage extends AppCompatActivity
-            implements AdapterView.OnItemSelectedListener {
+        Button btn = findViewById(R.id.button6);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            protected void onCreate(@Nullable Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.canvas_apron);
-
-                Spinner spinner = (Spinner) findViewById(R.id.spinner);
-                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                        R.array.match_array, android.R.layout.simple_spinner_dropdown_item);
-                adapter.setDropDownViewResource(android.layout.simple_spinner_dropdown_item);
-                spinner.setAdapter(adapter);
-                spinner.setOnItemSelectedListener(this);
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Added to cart!", Toast.LENGTH_SHORT).show();
             }
+        });
 
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                }
-            }
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
 
-            }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
         }
+    }
 
-        Button btn = findViewById(R.id.button6)
-                btn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(getApplicationContext(), "Added to cart!",Toast.LENGTH_SHORT).show();
-                    }
-                });
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
