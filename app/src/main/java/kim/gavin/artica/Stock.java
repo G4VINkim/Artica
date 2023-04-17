1ad275450f8fa46b03ee763d7bc866174db13f65
gavinkim <gavingvi0122@gmail.com>
2023-04-16 21:04:33 -0400

    change update

--- app/src/main/java/kim/gavin/artica/Stock.java
+++ app/src/main/java/kim/gavin/artica/Stock.java
@@ -1,14 +1,42 @@
 package kim.gavin.artica;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Stock extends AppCompatActivity {
+public class Stock extends AppCompatActivity
+    implements AdapterView.OnItemSelectedListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stock);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner7);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.location, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
